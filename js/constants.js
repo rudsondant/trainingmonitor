24883a0b1a958c4ffb9759313d57b877e4176b1d
RUMMENIGGE DANTAS <rudsondant@gmail.com>
2026-03-23 15:06:00 -0300

    Create constants.js
    Um arquivo para constantes. Deve crescer a medida que o front-end se aprimora.

---
+++ js/constants.js
@@ -0,0 +1,7 @@
+/* ============================
+   js/constants.js
+============================ */
+const APP_CONSTANTS = {
+  STORAGE_KEY: 'ficha_treino_profissional_v2',
+  THEME_KEY: 'ficha_treino_profissional_theme'
+};
