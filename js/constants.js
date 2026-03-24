9edaa99732f5541dc6ec298080195e8458e4beb0
RUMMENIGGE DANTAS <rudsondant@gmail.com>
2026-03-24 14:36:27 -0300

    Update constants.js
    novas constantes

--- js/constants.js
+++ js/constants.js
@@ -5,3 +5,20 @@ const APP_CONSTANTS = {
   STORAGE_KEY: 'ficha_treino_profissional_v2',
   THEME_KEY: 'ficha_treino_profissional_theme'
 };
+
+const WORKOUT_CONFIG = [
+  {
+    day: "Terça",
+    type: "Peito",
+    exercises: [
+      { name: "Supino", sets: 4 }
+    ]
+  }
+];
+
+const REHAB_PROTOCOL = [
+  { name: "Extensão joelho", reps: "3x20s" },
+  { name: "Elevação perna", reps: "3x15" },
+  { name: "Bird dog", reps: "3x10" },
+  { name: "Prancha", reps: "3x30s" }
+];
